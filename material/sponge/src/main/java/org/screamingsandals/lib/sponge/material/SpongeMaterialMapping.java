da3cb5d448d1f12ca52ef03ef082cd6a81e44a2d
Misat11 <misa.turek1@gmail.com>
2021-01-19 19:24:11 +0100

    not tested: sponge implementation of Material Resolver

--- material/sponge/src/main/java/org/screamingsandals/lib/sponge/material/SpongeMaterialMapping.java
+++ material/sponge/src/main/java/org/screamingsandals/lib/sponge/material/SpongeMaterialMapping.java
@@ -5,6 +5,7 @@ import org.screamingsandals.lib.material.MaterialMapping;
 import org.screamingsandals.lib.utils.Platform;
 import org.spongepowered.api.ResourceKey;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.data.Keys;
 import org.spongepowered.api.item.ItemType;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.registry.RegistryTypes;
@@ -23,9 +24,13 @@ public class SpongeMaterialMapping extends MaterialMapping {
 
         materialConverter
                 .registerW2P(ItemType.class, holder -> Sponge.getGame().registries().registry(RegistryTypes.ITEM_TYPE).findEntry(ResourceKey.resolve(holder.getPlatformName())).orElseThrow().value())
-                .registerW2P(ItemStack.class, holder -> ItemStack.of(Sponge.getGame().registries().registry(RegistryTypes.ITEM_TYPE).findEntry(ResourceKey.resolve(holder.getPlatformName())).orElseThrow().value(), (byte) 1))
+                .registerW2P(ItemStack.class, holder -> {
+                    var stack = ItemStack.of(Sponge.getGame().registries().registry(RegistryTypes.ITEM_TYPE).findEntry(ResourceKey.resolve(holder.getPlatformName())).orElseThrow().value(), (byte) 1);
+                    stack.offer(Keys.ITEM_DURABILITY, holder.getDurability());
+                    return stack;
+                })
                 .registerP2W(ItemType.class, material -> new MaterialHolder(Sponge.getGame().registries().registry(RegistryTypes.ITEM_TYPE).findValueKey(material).orElseThrow().getFormatted()))
-                .registerP2W(ItemStack.class, stack -> new MaterialHolder(Sponge.getGame().registries().registry(RegistryTypes.ITEM_TYPE).findValueKey(stack.getType()).orElseThrow().getFormatted()));
+                .registerP2W(ItemStack.class, stack -> new MaterialHolder(Sponge.getGame().registries().registry(RegistryTypes.ITEM_TYPE).findValueKey(stack.getType()).orElseThrow().getFormatted(), stack.getOrElse(Keys.ITEM_DURABILITY, 0)));
 
         Sponge.getGame().registries().registry(RegistryTypes.ITEM_TYPE).forEach(itemType ->
             materialMapping.put(itemType.key().getNamespace().equals(ResourceKey.MINECRAFT_NAMESPACE) ? itemType.key().getValue().toUpperCase() : itemType.key().getFormatted().toUpperCase(), new MaterialHolder(itemType.key().getFormatted()))
--- material/sponge/src/main/java/org/screamingsandals/lib/sponge/material/builder/SpongeItemFactory.java
+++ material/sponge/src/main/java/org/screamingsandals/lib/sponge/material/builder/SpongeItemFactory.java
@@ -1,18 +1,30 @@
 package org.screamingsandals.lib.sponge.material.builder;
 
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.screamingsandals.lib.material.Item;
 import org.screamingsandals.lib.material.builder.ItemFactory;
 import org.screamingsandals.lib.material.container.Container;
+import org.screamingsandals.lib.material.meta.PotionEffectMapping;
+import org.screamingsandals.lib.material.meta.PotionMapping;
 import org.screamingsandals.lib.sponge.material.SpongeMaterialMapping;
 import org.screamingsandals.lib.sponge.material.container.SpongeContainer;
 import org.screamingsandals.lib.sponge.material.meta.SpongeEnchantmentMapping;
 import org.screamingsandals.lib.sponge.material.meta.SpongePotionEffectMapping;
 import org.screamingsandals.lib.sponge.material.meta.SpongePotionMapping;
 import org.screamingsandals.lib.utils.InitUtils;
+import org.spongepowered.api.data.Keys;
+import org.spongepowered.api.effect.potion.PotionEffect;
+import org.spongepowered.api.item.enchantment.Enchantment;
 import org.spongepowered.api.item.inventory.Inventory;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.item.potion.PotionType;
 
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class SpongeItemFactory extends ItemFactory {
+
     public static void init() {
         ItemFactory.init(SpongeItemFactory::new);
     }
@@ -23,7 +35,158 @@ public class SpongeItemFactory extends ItemFactory {
         InitUtils.doIfNot(SpongeEnchantmentMapping::isInitialized, SpongeEnchantmentMapping::init);
         InitUtils.doIfNot(SpongePotionEffectMapping::isInitialized, SpongePotionEffectMapping::init);
 
-        // TODO
+        itemConverter
+                .registerW2P(ItemStack.class, item -> {
+                    var stack = item.getMaterial().as(ItemStack.class);
+                    stack.setQuantity(item.getAmount());
+                    /*if (item.getPlatformMeta() != null) {
+                        if (item.getPlatformMeta() instanceof ItemMeta) {
+                            stack.setItemMeta((ItemMeta) item.getPlatformMeta());
+                        }
+                    }*/
+
+                    if (item.getDisplayName() != null) {
+                        stack.offer(Keys.DISPLAY_NAME, LegacyComponentSerializer.legacySection().deserialize(item.getDisplayName()));
+                    }
+                    //if (item.getLocalizedName() != null) {
+                        // where is that?
+                    //}
+                    if (item.getCustomModelData() != null) {
+                        stack.offer(Keys.CUSTOM_MODEL_DATA, item.getCustomModelData());
+                    }
+                    // repair
+                    stack.offer(Keys.IS_UNBREAKABLE, item.isUnbreakable());
+                    if (item.getLore() != null) {
+                        stack.offer(Keys.LORE, item.getLore().stream().map(LegacyComponentSerializer.legacySection()::deserialize).collect(Collectors.toList()));
+                    }
+                    if (stack.supports(Keys.STORED_ENCHANTMENTS)) {
+                        stack.offer(Keys.STORED_ENCHANTMENTS, item.getEnchantments().stream().map(holder -> holder.as(Enchantment.class)).collect(Collectors.toList()));
+                    } else {
+                        stack.offer(Keys.APPLIED_ENCHANTMENTS, item.getEnchantments().stream().map(holder -> holder.as(Enchantment.class)).collect(Collectors.toList()));
+                    }
+                    if (item.getPotion() != null && stack.supports(Keys.POTION_TYPE)) {
+                        stack.offer(Keys.POTION_TYPE, item.getPotion().as(PotionType.class));
+                    }
+                    if (item.getPotionEffects() != null && stack.supports(Keys.POTION_EFFECTS)) {
+                        stack.offer(Keys.POTION_EFFECTS, item.getPotionEffects()
+                                .stream().map(holder -> holder.as(PotionEffect.class)).collect(Collectors.toList()));
+                    }
+                    if (item.getItemFlags() != null) {
+                        stack.offer(Keys.HIDE_ATTRIBUTES, item.getItemFlags().contains("HIDE_ATTRIBUTES"));
+                        stack.offer(Keys.HIDE_CAN_DESTROY, item.getItemFlags().contains("HIDE_DESTROYS"));
+                        // HIDE_DYE
+                        stack.offer(Keys.HIDE_ENCHANTMENTS, item.getItemFlags().contains("HIDE_ENCHANTS"));
+                        stack.offer(Keys.HIDE_CAN_PLACE, item.getItemFlags().contains("HIDE_PLACED_ON"));
+                        stack.offer(Keys.HIDE_MISCELLANEOUS, item.getItemFlags().contains("HIDE_POTION_EFFECTS"));
+                        stack.offer(Keys.HIDE_UNBREAKABLE, item.getItemFlags().contains("HIDE_UNBREAKABLE"));
+                    }
+
+                    return stack;
+                })
+                .registerP2W(ItemStack.class, stack -> {
+                    var item = new Item();
+                    var material = SpongeMaterialMapping.resolve(stack);
+                    if (material.isEmpty()) {
+                        return null; // WHAT??
+                    }
+
+                    item.setMaterial(material.get());
+                    item.setAmount(stack.getQuantity());
+                    //if (stack.getItemMeta() != null) {
+                    //    item.setPlatformMeta(stack.getItemMeta().clone());
+                    //}
+                    stack.get(Keys.DISPLAY_NAME).ifPresent(component ->
+                            item.setDisplayName(LegacyComponentSerializer.legacySection().serialize(component))
+                    );
+                    // localized name
+                    stack.get(Keys.CUSTOM_MODEL_DATA).ifPresent(item::setCustomModelData);
+                    // repair
+                    stack.get(Keys.IS_UNBREAKABLE).ifPresent(item::setUnbreakable);
+                    stack.get(Keys.LORE).ifPresent(components ->
+                            item.setLore(components.stream().map(LegacyComponentSerializer.legacySection()::serialize).collect(Collectors.toList()))
+                    );
+                    if (stack.supports(Keys.STORED_ENCHANTMENTS)) {
+                        stack.get(Keys.STORED_ENCHANTMENTS).ifPresent(enchantments ->
+                                enchantments.stream().map(SpongeEnchantmentMapping::resolve).forEach(en ->
+                                        item.getEnchantments().add(en.orElseThrow())
+                                )
+                        );
+                    } else {
+                        stack.get(Keys.APPLIED_ENCHANTMENTS).ifPresent(enchantments ->
+                                enchantments.stream().map(SpongeEnchantmentMapping::resolve).forEach(en ->
+                                        item.getEnchantments().add(en.orElseThrow())
+                                )
+                        );
+                    }
+                    stack.get(Keys.HIDE_ATTRIBUTES).ifPresent(aBoolean -> {
+                        if (aBoolean) {
+                            if (item.getItemFlags() != null) {
+                                item.setItemFlags(new ArrayList<>());
+                            }
+                            item.getItemFlags().add("HIDE_ATTRIBUTES");
+                        }
+                    });
+                    stack.get(Keys.HIDE_CAN_DESTROY).ifPresent(aBoolean -> {
+                        if (aBoolean) {
+                            if (item.getItemFlags() != null) {
+                                item.setItemFlags(new ArrayList<>());
+                            }
+                            item.getItemFlags().add("HIDE_DESTROYS");
+                        }
+                    });
+                    /*stack.get(Keys.HIDE_DYE).ifPresent(aBoolean -> {
+                        if (item.getItemFlags() != null) {
+                            item.setItemFlags(new ArrayList<>());
+                        }
+                        item.getItemFlags().add("HIDE_DYE");
+                    });*/
+                    stack.get(Keys.HIDE_ENCHANTMENTS).ifPresent(aBoolean -> {
+                        if (aBoolean) {
+                            if (item.getItemFlags() != null) {
+                                item.setItemFlags(new ArrayList<>());
+                            }
+                            item.getItemFlags().add("HIDE_ENCHANTS");
+                        }
+                    });
+                    stack.get(Keys.HIDE_CAN_PLACE).ifPresent(aBoolean -> {
+                        if (aBoolean) {
+                            if (item.getItemFlags() != null) {
+                                item.setItemFlags(new ArrayList<>());
+                            }
+                            item.getItemFlags().add("HIDE_PLACED_ON");
+                        }
+                    });
+                    stack.get(Keys.HIDE_MISCELLANEOUS).ifPresent(aBoolean -> {
+                        if (aBoolean) {
+                            if (item.getItemFlags() != null) {
+                                item.setItemFlags(new ArrayList<>());
+                            }
+                            item.getItemFlags().add("HIDE_POTION_EFFECTS");
+                        }
+                    });
+                    stack.get(Keys.HIDE_UNBREAKABLE).ifPresent(aBoolean -> {
+                        if (aBoolean) {
+                            if (item.getItemFlags() != null) {
+                                item.setItemFlags(new ArrayList<>());
+                            }
+                            item.getItemFlags().add("HIDE_UNBREAKABLE");
+                        }
+                    });
+                    stack.get(Keys.POTION_TYPE).flatMap(PotionMapping::resolve).ifPresent(item::setPotion);
+                    stack.get(Keys.POTION_EFFECTS).ifPresent(potionEffects -> {
+                        var list = potionEffects.stream()
+                                .map(PotionEffectMapping::resolve)
+                                .filter(Optional::isPresent)
+                                .map(Optional::get)
+                                .collect(Collectors.toList());
+                        if (!list.isEmpty()) {
+                            item.setPotionEffects(list);
+                        }
+                    });
+
+                    return item;
+
+                });
     }
 
     @Override
