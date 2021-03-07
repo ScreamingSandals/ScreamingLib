2d254ca4ea7fcc66e214da7571e8c530b0e3f5e4
iamceph <frantisek@nvsd.dev>
2021-03-07 11:33:33 +0100

    Am I supposed to acces this with reflection?
    @pronze

--- player/common/src/main/java/org/screamingsandals/lib/player/event/SPlayerDamageByEntityEvent.java
+++ player/common/src/main/java/org/screamingsandals/lib/player/event/SPlayerDamageByEntityEvent.java
@@ -1,12 +1,14 @@
 package org.screamingsandals.lib.player.event;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 import org.screamingsandals.lib.entity.EntityBasic;
 import org.screamingsandals.lib.player.PlayerWrapper;
 
 @EqualsAndHashCode(callSuper = false)
 public class SPlayerDamageByEntityEvent extends SPlayerDamageEvent {
+    @Getter
     private final EntityBasic damager;
 
     public SPlayerDamageByEntityEvent(@NotNull final EntityBasic damager,
