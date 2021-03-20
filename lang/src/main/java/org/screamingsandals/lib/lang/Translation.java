c5d6f7aa151f57eabbb4fc930311ba93202901cc
iamceph <frantisek@nvsd.dev>
2021-03-20 17:58:19 +0100

    Rather create new instance every time.

--- lang/src/main/java/org/screamingsandals/lib/lang/Translation.java
+++ lang/src/main/java/org/screamingsandals/lib/lang/Translation.java
@@ -6,21 +6,17 @@ import net.kyori.adventure.text.format.NamedTextColor;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @Data
 public final class Translation {
-    private final List<String> keys = new CopyOnWriteArrayList<>();
+    private final List<String> keys = new LinkedList<>();
     private final Component fallback;
 
     private Translation(Collection<String> keys, Component fallback) {
         this.keys.addAll(keys);
         this.fallback = fallback;
-    }
-
-    public static Translation of(Collection<String> keys, Component fallback) {
-        return new Translation(keys, fallback);
     }
 
     public static Translation of(String... keys) {
@@ -31,13 +27,28 @@ public final class Translation {
         return of(keys, Component.text(String.join(".", keys)).color(NamedTextColor.RED));
     }
 
-    public Translation join(String... key) {
-        keys.addAll(Arrays.asList(key));
-        return this;
+    public static Translation of(Collection<String> keys, Component fallback) {
+        return new Translation(keys, fallback);
     }
 
-    public Translation join(Collection<String> key) {
-        keys.addAll(key);
-        return this;
+    public Translation join(String... key) {
+        final var copied = new LinkedList<>(keys);
+        copied.addAll(Arrays.asList(key));
+
+        return of(copied);
+    }
+
+    public Translation join(Collection<String> keys) {
+        final var copied = new LinkedList<>(this.keys);
+        copied.addAll(keys);
+
+        return of(copied, Component.text(String.join(".", keys)).color(NamedTextColor.RED));
+    }
+
+    public Translation join(Collection<String> keys, Component fallback) {
+        final var copied = new LinkedList<>(this.keys);
+        copied.addAll(keys);
+
+        return of(copied, fallback);
     }
 }
