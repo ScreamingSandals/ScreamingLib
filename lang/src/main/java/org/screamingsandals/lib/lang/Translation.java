74ee9e672ba12d7ab216501c12708c899bd99004
iamceph <frantisek@nvsd.dev>
2021-03-20 17:55:35 +0100

    Add join methods to Translation

--- lang/src/main/java/org/screamingsandals/lib/lang/Translation.java
+++ lang/src/main/java/org/screamingsandals/lib/lang/Translation.java
@@ -1,31 +1,43 @@
 package org.screamingsandals.lib.lang;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Data
-@RequiredArgsConstructor(staticName = "of")
 public final class Translation {
-    private final String[] keys;
+    private final List<String> keys = new CopyOnWriteArrayList<>();
     private final Component fallback;
 
-    public static Translation ofString(String key) {
-        return of(key.split("\\."));
-    }
-
-    public static Translation of(String... keys) {
-        return of(keys, Component.text(String.join(".", keys)).color(NamedTextColor.RED));
-    }
-
-    public static Translation of(Collection<String> keys) {
-        return of(keys.toArray(String[]::new), Component.text(String.join(".", keys)).color(NamedTextColor.RED));
+    private Translation(Collection<String> keys, Component fallback) {
+        this.keys.addAll(keys);
+        this.fallback = fallback;
     }
 
     public static Translation of(Collection<String> keys, Component fallback) {
-        return of(keys.toArray(String[]::new), fallback);
+        return new Translation(keys, fallback);
+    }
+
+    public static Translation of(String... keys) {
+        return of(Arrays.asList(keys), Component.text(String.join(".", keys)).color(NamedTextColor.RED));
+    }
+
+    public static Translation of(Collection<String> keys) {
+        return of(keys, Component.text(String.join(".", keys)).color(NamedTextColor.RED));
+    }
+
+    public Translation join(String... key) {
+        keys.addAll(Arrays.asList(key));
+        return this;
+    }
+
+    public Translation join(Collection<String> key) {
+        keys.addAll(key);
+        return this;
     }
 }
