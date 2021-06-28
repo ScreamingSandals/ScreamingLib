ba889694a3685e2cd860465e8167d57596c368da
pronze <pronzemc@gmail.com>
2021-06-28 17:48:05 +0400

    Fix for velocity npe

--- utils/bukkit/src/main/java/org/screamingsandals/lib/bukkit/utils/nms/entity/EntityNMS.java
+++ utils/bukkit/src/main/java/org/screamingsandals/lib/bukkit/utils/nms/entity/EntityNMS.java
@@ -148,6 +148,9 @@ public class EntityNMS {
 
 	public Vector3D getVelocity() {
 		final var mot = Reflect.getMethod(handler, "getMot,func_213322_ci").invoke();
+		if (mot == null) {
+			return new Vector3D(0, 0, 0);
+		}
 		final var bukkitVector = (Vector) Reflect.getMethod(ClassStorage.NMS.CraftVector, "toBukkit").invokeStatic(mot);
 		return new Vector3D(
 				bukkitVector.getX(),
