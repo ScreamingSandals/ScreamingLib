86cb7c656dd74ba813394d35b1b24118fc3b332e
Misat11 <misa.turek1@gmail.com>
2021-07-10 13:19:00 +0200

    fix building

--- npc/bukkit/src/main/java/org/screamingsandals/lib/bukkit/npc/BukkitNPCManager.java
+++ npc/bukkit/src/main/java/org/screamingsandals/lib/bukkit/npc/BukkitNPCManager.java
@@ -9,7 +9,7 @@ import org.screamingsandals.lib.npc.AbstractNPC;
 import org.screamingsandals.lib.npc.NPCManager;
 import org.screamingsandals.lib.npc.event.NPCTouchEvent;
 import org.screamingsandals.lib.player.PlayerMapper;
-import org.screamingsandals.lib.player.event.SPlayerLeaveEvent;
+import org.screamingsandals.lib.event.player.SPlayerLeaveEvent;
 import org.screamingsandals.lib.utils.Controllable;
 import org.screamingsandals.lib.utils.annotations.Service;
 import org.screamingsandals.lib.utils.reflect.Reflect;
