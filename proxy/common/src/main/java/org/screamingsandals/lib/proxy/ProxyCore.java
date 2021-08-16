a970af20b293d3828639adcc22dc7d77d84f63c6
Misat11 <misa.turek1@gmail.com>
2021-08-16 21:56:28 +0200

    another phone coding moment

--- proxy/common/src/main/java/org/screamingsandals/lib/proxy/ProxyCore.java
+++ proxy/common/src/main/java/org/screamingsandals/lib/proxy/ProxyCore.java
@@ -3,7 +3,12 @@ package org.screamingsandals.lib.proxy;
 import org.screamingsandals.lib.event.EventManager;
 import org.screamingsandals.lib.utils.annotations.ServiceDependencies;
 import org.screamingsandals.lib.utils.annotations.internal.InternalCoreService;
+import org.screamingsandals.lib.utils.annotations.AbstractService;
 
+@AbstractService(
+        pattern = "^(?<basePackage>.+)\\.(?<className>.+)$",
+        replaceRule = "{basePackage}.{platform}.{Platform}{className}"
+)
 @ServiceDependencies(dependsOn = {
         EventManager.class,
         ProxiedPlayerMapper.class
