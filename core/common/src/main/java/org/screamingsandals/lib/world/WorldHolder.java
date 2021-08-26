2ab72700aa29b6c0cc1dde955f88d67d1ec62737
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-08-26 21:29:01 +0200

    getEntitiesByClass() and getEntities() (#7)

--- core/common/src/main/java/org/screamingsandals/lib/world/WorldHolder.java
+++ core/common/src/main/java/org/screamingsandals/lib/world/WorldHolder.java
@@ -3,6 +3,7 @@ package org.screamingsandals.lib.world;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import org.screamingsandals.lib.entity.EntityBasic;
 import org.screamingsandals.lib.utils.Wrapper;
 import org.screamingsandals.lib.world.chunk.ChunkHolder;
 import org.screamingsandals.lib.world.difficulty.DifficultyHolder;
@@ -10,6 +11,7 @@ import org.screamingsandals.lib.world.dimension.DimensionHolder;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -30,6 +32,10 @@ public interface WorldHolder extends Wrapper, Serializable {
     Optional<ChunkHolder> getChunkAt(int x, int z);
 
     Optional<ChunkHolder> getChunkAt(LocationHolder location);
+
+    List<EntityBasic> getEntities();
+
+    <T extends EntityBasic> List<T> getEntitiesByClass(Class<T> cls);
 
     class WorldHolderTypeAdapter extends TypeAdapter<WorldHolder> {
         @Override
