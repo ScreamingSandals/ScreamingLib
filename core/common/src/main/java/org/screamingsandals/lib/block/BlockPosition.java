89fb190f3c418862fb6a19876a8660cc9a278b82
pronze <pronzemc@gmail.com>
2021-12-27 20:54:43 +0400

    Complete BlockPosition#toLong() method

--- core/common/src/main/java/org/screamingsandals/lib/block/BlockPosition.java
+++ core/common/src/main/java/org/screamingsandals/lib/block/BlockPosition.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.screamingsandals.lib.Server;
 
 @Data
 @NoArgsConstructor
@@ -14,9 +15,11 @@ public class BlockPosition {
     protected int y;
     protected int z;
 
-    // TODO:
     public static long toLong(int x, int y, int z) {
-        return 0;
+        if (Server.isVersion(1, 14)) {
+            return ((long) (x & 0x3FFFFFF) << 38) | ((long) (z & 0x3FFFFFF) << 12) | (y & 0xFFF);
+        }
+        return ((long) (x & 0x3FFFFFF) << 38) | ((long) (y & 0xFFF) << 26) | (z & 0x3FFFFFF);
     }
 
     public long asLong() {
