bfb84da2a034bd36aaf0e43c5999dd9d8ef27bb2
Misat11 <misa.turek1@gmail.com>
2021-12-30 19:24:18 +0100

    fixed another proto missing error and fixed BukkitPotionEffectHolder for spigot and older paper

--- utils/common/src/main/java/org/screamingsandals/lib/utils/ProtoWrapper.java
+++ utils/common/src/main/java/org/screamingsandals/lib/utils/ProtoWrapper.java
@@ -6,7 +6,7 @@ import com.google.protobuf.Message;
  * A wrapper for .proto conversions.
  * @param <T> .proto type of this class
  */
-public interface ProtoWrapper<T extends Message> {
+public interface ProtoWrapper<T/* extends Message*/> {
 
     /**
      * Wraps current wrapper into a .proto class
