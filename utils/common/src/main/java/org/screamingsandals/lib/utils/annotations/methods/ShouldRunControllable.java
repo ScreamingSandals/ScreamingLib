75decfbef68e983f8a45c12fe1de8971d38090a4
pronze <16319848+pronze@users.noreply.github.com>
2022-01-01 21:17:19 +0400

    Fix javadoc mistake

--- utils/common/src/main/java/org/screamingsandals/lib/utils/annotations/methods/ShouldRunControllable.java
+++ utils/common/src/main/java/org/screamingsandals/lib/utils/annotations/methods/ShouldRunControllable.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 /**
  * The ShouldRunControllable annotation is used to mark method in {@link org.screamingsandals.lib.utils.annotations.Service}
  * which returns boolean indicating whether or not controllable should be used.
- * Each service can have only ShouldRunControllable method.
+ * Each service can have only one ShouldRunControllable method.
  */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.METHOD)
