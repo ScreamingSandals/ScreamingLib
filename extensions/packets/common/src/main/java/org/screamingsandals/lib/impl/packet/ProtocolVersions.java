064d01bcf2418daede84b8966a1d2a2d3ec6e13e
Misat11 <20199703+Misat11@users.noreply.github.com>
2025-09-30 23:12:58 +0200

    build: 1.21.9 nms support (still missing a lot of stuff)

--- extensions/packets/common/src/main/java/org/screamingsandals/lib/impl/packet/ProtocolVersions.java
+++ extensions/packets/common/src/main/java/org/screamingsandals/lib/impl/packet/ProtocolVersions.java
@@ -24,4 +24,5 @@ public class ProtocolVersions {
     public static final int V1_20_5 = 766;
     public static final int V1_21_2 = 768;
     public static final int V1_21_5 = 770;
+    public static final int V1_21_9 = 773;
 }
