77abd7cbc6baaa8d2b871a8d445866b3e6d9c3a1
Misat11 <20199703+Misat11@users.noreply.github.com>
2025-12-31 18:25:59 +0100

    build: rewrite includeModule function

--- buildSrc/src/main/kotlin/org/screamingsandals/lib/gradle/includeModule.kt
+++ buildSrc/src/main/kotlin/org/screamingsandals/lib/gradle/includeModule.kt
@@ -1,14 +1,39 @@
 package org.screamingsandals.lib.gradle
 
 import org.gradle.api.Project
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 fun Project.includeModule(module: String) {
+    if (!name.endsWith("-common")) {
+        throw UnsupportedOperationException("This method can be used only in common modules!")
+    }
 
-    // TODO: ACTUALLY REIMPLEMENT THE FUNCTION!!!!
+    dependencies {
+        "api"(project(":${module}-common"))
+    }
 
-    @Suppress("UNCHECKED_CAST")
-    val includeModule = project.extensions.extraProperties["includeModule"]
-            as groovy.lang.Closure<*>
+    val baseName = name.removeSuffix("-common")
+    val moduleRegex = Regex("$baseName-[^-]+")
 
-    includeModule.call(module)
+    rootProject.subprojects.forEach { sub ->
+        if (!sub.name.endsWith("-common") && moduleRegex.matches(sub.name)) {
+            val platform = sub.name.substringAfter("-")
+            val moduleProject = rootProject.findProject(":$module-$platform")
+
+            if (moduleProject != null) {
+                sub.dependencies {
+                    "api"(moduleProject)
+
+                    if (moduleProject.name == "core-bukkit") {
+                        // Fix another source sets missing when compiling
+                        val sourceSets = moduleProject.extensions.getByType<SourceSetContainer>()
+
+                        "compileOnly"(sourceSets.named("main").get().output)
+                    }
+                }
+            }
+        }
+    }
 }
